199cf1990f713de290364dcb2ceb42684bff5586
Gauthier Delamarre <gauthier@objective-php.org>
2016-07-10 12:43:29 +0200

    Add ApiErrorHandler middleware

---
+++ src/Api/ApiErrorHandler.php
@@ -0,0 +1,34 @@
+<?php
+    
+    namespace Fei\ApiServer\Api;
+
+    use ObjectivePHP\Application\ApplicationInterface;
+    use ObjectivePHP\Application\Middleware\AbstractMiddleware;
+    use Zend\Diactoros\Response;
+    use Zend\Diactoros\Response\JsonResponse;
+    use Zend\Diactoros\Response\SapiEmitter;
+
+    class ApiErrorHandler extends AbstractMiddleware
+    {
+
+
+        public function run(ApplicationInterface $app)
+        {
+
+            $e = $app->getException()->getPrevious() ?: $app->getException();
+
+            $code = $e->getCode() && $e->getCode() > 199 && $e->getCode() < 599 ?: 500;
+            (new SapiEmitter())->emit(
+             new JsonResponse(
+                [
+                    'code' => -1,
+                    'error' => $e->getMessage(),
+                    'type' => get_class($e),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine()
+                ],
+                $code
+            ));
+
+        }
+    }
