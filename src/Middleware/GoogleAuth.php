6e0e09457f9da36d3babbb86ee7505b55765303a
Vincent Simonin <vsi@opcoding.eu>
2016-08-24 18:32:52 +0200

    Added a way to configure GoogleAuth SPID

--- src/Middleware/GoogleAuth.php
+++ src/Middleware/GoogleAuth.php
@@ -2,6 +2,8 @@
 
 namespace Fei\ApiServer\Middleware;
 
+use ObjectivePHP\Application\ApplicationInterface;
+use ObjectivePHP\Application\Config\Param;
 use ObjectivePHP\Application\Middleware\AbstractMiddleware;
 
 /**
@@ -11,12 +13,20 @@ use ObjectivePHP\Application\Middleware\AbstractMiddleware;
  */
 class GoogleAuth extends AbstractMiddleware
 {
-    public function run()
+    public function run(ApplicationInterface $app)
     {
+        $spid = $app->getConfig()->subset(Param::class)->get('spid');
+
+        if (!$spid) {
+            throw new \LogicException(
+                'SPID should not be empty. Please add `new Param(\'spid\', \'yourspid\')` in your configuration.'
+            );
+        }
+
         if (!getenv('REMOTE_USER')) {
             header(
                 'Location: ' .
-                'https://accounts.google.com/o/saml2/initsso?idpid=C00mtquyv&spid=1023093070760&forceauthn=false'
+                'https://accounts.google.com/o/saml2/initsso?idpid=C00mtquyv&spid=' . $spid . '&forceauthn=false'
             );
             exit();
         }
