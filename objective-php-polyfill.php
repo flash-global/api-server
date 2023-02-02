f15998132453ff915c20ab3e4ecccd7f36a55e11
amaraSanoh <sanohawa@gmail.com>
2023-02-02 19:06:04 +0100

    update class aliases

--- objective-php-polyfill.php
+++ objective-php-polyfill.php
@@ -1,5 +1,13 @@
 <?php
 
 if (!interface_exists('ObjectivePHP\Gateway\Entity\EntityInterface')) {
-    class_alias('Fei\Entities\ObjectivePHP\Gateway\Entity\EntityInterface', 'ObjectivePHP\Gateway\Entity\EntityInterface');
+    class_alias('Fei\ApiServer\ObjectivePHP\Gateway\Entity\EntityInterface', 'ObjectivePHP\Gateway\Entity\EntityInterface');
+}
+
+if (!interface_exists('ObjectivePHP\Gateway\ResultSet\PaginatedResultSetInterface')) {
+    class_alias('Fei\ApiServer\ObjectivePHP\Gateway\ResultSet\PaginatedResultSetInterface', 'ObjectivePHP\Gateway\ResultSet\PaginatedResultSetInterface');
+}
+
+if (!interface_exists('ObjectivePHP\Gateway\ResultSet\ResultSetInterface')) {
+    class_alias('Fei\ApiServer\ObjectivePHP\Gateway\ResultSet\ResultSetInterface', 'ObjectivePHP\Gateway\ResultSet\ResultSetInterface');
 }
